4ddd1d51247171d50c8fd7dc530ef8e918f9a29e
amiceli <amiceli@letsignit.com>
2023-11-12 18:04:06 +0100

    feat: add App vue endpoint

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="App">
+        <AppHeader />
+        <AppDescription />
+        <AppDetails />
+        <AppDev />
+        <AppLavori />
+        <AppJobs />
+        <AppForever />
+    </div>
+</template>
+
+<script setup>
+    import AppDescription from './components/AppDescription.vue'
+    import AppHeader from './components/AppHeader.vue'
+    import AppDetails from './components/AppDetails.vue'
+    import AppDev from './components/AppDev.vue'
+    import AppLavori from './components/AppLavori.vue'
+    import AppJobs from './components/AppJobs.vue'
+    import AppForever from './components/AppForever.vue'
+</script>
+
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;500;600&display=swap');
+
+    @font-face {
+        font-family: 'Kbanavoxlost';
+        src: url('../src/assets/Kbanavoxlost-gajq.ttf') format('woff');
+    }
+
+    html,
+    body {
+        font-family: 'Exo 2', sans-serif;
+        color: #d8dee9;
+        background: #2E3440;
+    }
+
+    .for--rod {
+        font-family: 'Kbanavoxlost';
+        font-weight: 800;
+    }
+</style>
+
+<style lang="scss" scoped>
+    .App {
+        width: 90%;
+        margin: auto;
+        padding-top: 100px;
+        padding-bottom: 100px;
+    }
+</style>
