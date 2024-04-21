d6a0a373949087adbc309fbf120add62f5523dfb
amiceli <antoine@miceli.click>
2024-04-21 21:47:28 +0200

    chore: update plausible config for domain

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,25 @@
-import {
-    fileURLToPath, URL,
-} from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { URL, fileURLToPath } from "node:url"
+import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins : [
+    plugins: [
         vue(),
         {
-            name : `html-inject-nonce-into-script-tag`,
-            enforce : `post`,
-            transformIndexHtml (html) {
-                const script = `<script async type="application/javascript" src="https://plausible.volpe.xyz/script.js" data-website-id="b8875c72-2935-418c-8464-215526912cf7" ></script>`
+            name: `html-inject-nonce-into-script-tag`,
+            enforce: `post`,
+            transformIndexHtml(html) {
+                const script = `<script async type="application/javascript" src="https://plausible.volpe.xyz/script.js" data-website-id="b8875c72-2935-418c-8464-215526912cf7" data-domains="miceli.click"></script>`
 
                 return html.replace(`<!-- production -->`, script)
             },
         },
     ],
-    css : {
-        preprocessorOptions : {
-            scss : {
-                additionalData : `
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
                     @mixin onlyMobile () {
                         @media (pointer:none),
                         (pointer:coarse) {
@@ -32,9 +30,9 @@ export default defineConfig({
             },
         },
     },
-    resolve : {
-        alias : {
-            '@' : fileURLToPath(new URL(`./src`, import.meta.url)),
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL(`./src`, import.meta.url)),
         },
     },
 })
