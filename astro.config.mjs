45d36325f5d35474461686e64a349b1d5ee5e7aa
amiceli <antoine@miceli.click>
2024-08-25 16:15:06 +0200

    chore: add astro mdx deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config"
-
+import mdx from "@astrojs/mdx"
 import vue from "@astrojs/vue"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +20,5 @@ export default defineConfig({
             },
         },
     },
-    integrations: [vue()],
+    integrations: [vue(), mdx()],
 })
