5ccd2853db47431b7ed08cf5de94884e7527067d
amiceli <antoine@miceli.click>
2026-09-06 16:16:23 +0200

    fix: umami error

--- src/umami.js
+++ src/umami.js
@@ -5,10 +5,15 @@ const isDev = import.meta.env.MODE === `development`
 function trackEvent(eventName, eventProperty) {
     if (isDev) {
         console.debug(`mock_umami_track_event`, eventName, eventProperty)
+
         return Promise.resolve()
     }
 
-    return umami.track(eventName, eventProperty)
+    if (typeof umami === `undefined`) {
+        return Promise.resolve()
+    }
+
+    return Promise.resolve(umami.track(eventName, eventProperty))
 }
 
 export function trackLink(url) {
